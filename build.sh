e377af0141707b89f3908ea9c788d4305e4059c1
stani-m <89606745+stani-m@users.noreply.github.com>
2021-10-17 01:18:07 +0200

    Renamed the project to "VAII semestral project"

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --target web --no-typescript --out-dir www/ target/wasm32-unknown-unknown/release/wasm_sample.wasm
+wasm-bindgen --target web --no-typescript --out-dir www/ target/wasm32-unknown-unknown/release/vaii_sem_project.wasm
