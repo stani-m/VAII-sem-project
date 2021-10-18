514619f32a6ee1b446fe156b214078191f12ca69
stani-m <89606745+stani-m@users.noreply.github.com>
2021-10-18 19:52:12 +0200

    Added release build option with better optimization

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --target web --no-typescript --out-dir www/ target/wasm32-unknown-unknown/release/vaii_sem_project.wasm
+if [ "$1" = release ]; then
+  cargo build --release --target wasm32-unknown-unknown
+  wasm-bindgen --target web --no-typescript --out-dir www/ target/wasm32-unknown-unknown/release/vaii_sem_project.wasm
+  wasm-opt -O --converge -o www/vaii_sem_project_bg.wasm www/vaii_sem_project_bg.wasm
+else
+  cargo build --target wasm32-unknown-unknown
+  wasm-bindgen --target web --no-typescript --out-dir www/ target/wasm32-unknown-unknown/debug/vaii_sem_project.wasm
+fi
