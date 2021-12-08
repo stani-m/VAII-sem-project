430453765da2f0207a2fc1589f6be9d91e2b1043
stani-m <89606745+stani-m@users.noreply.github.com>
2021-12-09 00:31:54 +0100

    Started using custom cargo profiles

--- game/build.sh
+++ game/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if [ "$1" = release ]; then
-  cargo build --release
-  wasm-bindgen --target web --no-typescript --out-dir ../www/ target/wasm32-unknown-unknown/release/game.wasm
+  cargo build --profile deploy
+  wasm-bindgen --target web --no-typescript --out-dir ../www/ target/wasm32-unknown-unknown/deploy/game.wasm
   wasm-opt -O --converge -o ../www/game_bg.wasm ../www/game_bg.wasm
 else
   cargo build
-  wasm-bindgen --target web --no-typescript --out-dir ../www/ target/wasm32-unknown-unknown/debug/game.wasm
+  wasm-bindgen --target web --no-typescript --out-dir ../www/ target/wasm32-unknown-unknown/release/game.wasm
 fi
