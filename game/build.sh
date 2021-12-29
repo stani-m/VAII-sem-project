662d62d1fb0bb7542115435f9046d1229b5851ae
stani-m <89606745+stani-m@users.noreply.github.com>
2021-12-29 22:44:17 +0100

    Improved build configurations and removed wasm-opt build dependency

--- game/build.sh
+++ game/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-if [ "$1" = release ]; then
+if [ "$1" = deploy ]; then
   cargo build --profile deploy
-  wasm-bindgen --target web --no-typescript --out-dir ../www/ target/wasm32-unknown-unknown/deploy/game.wasm
-  wasm-opt -O --converge -o ../www/game_bg.wasm ../www/game_bg.wasm
+  wasm-bindgen --target web --no-typescript --remove-name-section --remove-producers-section --out-dir ../www/ target/wasm32-unknown-unknown/deploy/game.wasm
 else
   cargo build --release
-  wasm-bindgen --target web --no-typescript --out-dir ../www/ target/wasm32-unknown-unknown/release/game.wasm
+  wasm-bindgen --target web --no-typescript --remove-producers-section --out-dir ../www/ target/wasm32-unknown-unknown/release/game.wasm
 fi
